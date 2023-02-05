3ef7cb4a96939871e5a1db2a5e8be6c7daadd6f0
thanosoncode <thanosoncode@gmail.com>
2023-02-05 17:12:42 +0200

    added .envç

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4444/api",
+  baseURL: import.meta.env.PROD
+    ? "https://workouts-api-fzz9.onrender.com/api"
+    : import.meta.env.BASE_URL,
 });
 
 export default axiosInstance;
