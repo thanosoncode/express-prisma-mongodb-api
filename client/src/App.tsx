914921dd4c3f7179f60e2f5b83f40ed370c5b9bd
thanosoncode <thanosoncode@gmail.com>
2023-02-10 16:35:18 +0200

    added test setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,33 +9,31 @@ import RootLayout from "./components/RootLayout";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
-import { Box } from "@mui/material";
 import Workouts from "./pages/workouts/Workouts.component";
 import Progression from "./pages/progression/Progression.component";
 import Error from "./components/error/Error.component";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />} errorElement={<Error />}>
+      <Route index element={<Home />} errorElement={<Error />} />
+      <Route
+        path="/my-workouts"
+        element={<Workouts />}
+        errorElement={<Error />}
+      />
+      <Route
+        path="/progression"
+        element={<Progression />}
+        errorElement={<Error />}
+      />
+    </Route>
+  )
+);
 const App = () => {
   const queryClient = new QueryClient();
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<RootLayout />} errorElement={<Error />}>
-        <Route index element={<Home />} errorElement={<Error />} />
-        <Route
-          path="/my-workouts"
-          element={<Workouts />}
-          errorElement={<Error />}
-        />
-        <Route
-          path="/progression"
-          element={<Progression />}
-          errorElement={<Error />}
-        />
-      </Route>
-    )
-  );
 
   return (
     <QueryClientProvider client={queryClient}>
--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.PROD
+  baseURL: process.env.PROD
     ? "https://workouts-api-fzz9.onrender.com/api"
-    : import.meta.env.VITE_REACT_BASE_URL,
+    : process.env.VITE_REACT_BASE_URL,
 });
 
 export default axiosInstance;
